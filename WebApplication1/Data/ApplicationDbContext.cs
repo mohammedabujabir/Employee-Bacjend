39064b748e5943d4839b327fb1dbee9e590ce9de
mohammedabujabir <mohammed.tayser.2016@gmail.com>
2024-05-07 11:22:52 -0700

    Employee-Backend

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Employee> Employees {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=COMPANY10;trusted_connection=true;trustservercertificate=true;");
+        }
+    }
+}
